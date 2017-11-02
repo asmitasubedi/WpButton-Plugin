571d4e98b88f3cedb6144123a996c04a94cfce1d
Abhishek Gupta <abhishek.gupta@deerwalk.edu.np>
2017-11-02 17:37:27 +0545

    push

--- tlbtn-single-post-ref.php
+++ tlbtn-single-post-ref.php
@@ -1,195 +1,240 @@
-<?php
-/**
- * The template for displaying all single posts
- *
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
- *
- * @package WordPress
- * @subpackage Twenty_Seventeen
- * @since 1.0
- * @version 1.0
- */
+<article <?php hybrid_attr( 'post' ); ?>>
 
-get_header(); ?>
+	<?php if ( has_post_format( 'video' ) ) : ?>
+		<div class="entry-image video">
+			<?php echo hybrid_media_grabber( array( 'type' => 'video', 'split_media' => true ) ); ?>
+		</div>
+	<?php elseif ( has_post_format( 'audio' ) ) : ?>
+		<div class="entry-image audio">
+			<?php echo hybrid_media_grabber( array( 'type' => 'audio', 'split_media' => true ) ); ?>
+		</div>
+	<?php elseif ( has_post_format( 'image' ) )	: // do nothing ?>
+	<?php elseif ( has_post_format( 'gallery' ) ) : ?>
+		<?php echo sitebox_get_format_gallery(); // Get the gallery ?>
+	<?php else : ?>
 
+		<?php
+		// Get the data set in customizer
+		$enable = sitebox_mod( 'sitebox-post-thumbnail' );
 
-<style>
+		if ( $enable && has_post_thumbnail() ) :
 
-	.tlBtn-modalDialog {
-		position: fixed;
-		font-family: Arial, Helvetica, sans-serif;
-		top: 0;
-		right: 0;
-		bottom: 0;
-		left: 0;
-		background: rgba(0, 0, 0, 0.8);
-		z-index: 99999;
-		opacity:0;
-		-webkit-transition: opacity 400ms ease-in;
-		-moz-transition: opacity 400ms ease-in;
-		transition: opacity 400ms ease-in;
-		pointer-events: none;
-	}
-	.tlBtn-modalDialog:target {
-		opacity:1;
-		pointer-events: auto;
-	}
-	.tlBtn-modalDialog > div {
-		width: 600px;
-		position: relative;
-		margin: 5% auto;
-		padding: 0;
-		border-radius: 0px;
-		background: #fff;
-	}
-	.tlBtn-close {
-		background: #606061;
-		color: #FFFFFF;
-		line-height: 25px;
-		position: absolute;
-		right: -12px;
-		text-align: center;
-		top: -10px;
-		width: 24px;
-		text-decoration: none;
-		font-weight: bold;
-		-webkit-border-radius: 12px;
-		-moz-border-radius: 12px;
-		border-radius: 12px;
-		-moz-box-shadow: 1px 1px 3px #000;
-		-webkit-box-shadow: 1px 1px 3px #000;
-		box-shadow: 1px 1px 3px #000;
-	}
-	.tlBtn-close:hover {
-		background: #e41212;
-		color: #fff;
-	}
-	div#tlbtn-product-title p {
-		margin: 0;
-		font-size: 18px;
-		font-weight: bold;
-		color: #383838;
-	}
-	div#tlbtn-product-title {
-		background-color: #f8f8f8 !important;
-		border-bottom: #f5f5f5 1px solid;
-		padding: 12px 15px !important;
-		box-shadow: 0 0 6px rgba(0,0,0,0.2);
-		background: #d5d5d5;
-	}
-	div#tltbn-main-form {
-		padding: 15px;
-	}
-	input#tlbtn-sumbit {
-		background: #e41121;
-		border-radius: 0;
-	}
+			// for has_sidebar
+			$size = 'post-thumbnail';
 
-</style>
-<div class="wrap">
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
-				<?php
-			/* Start the Loop */
-			while ( have_posts() ) : the_post();
-
-				get_template_part( 'template-parts/post/content', get_post_format() );
-
-				// If comments are open or we have at least one comment, load up the comment template.
-				if ( comments_open() || get_comments_number() ) :
-					comments_template();
-				endif;
-
-				the_post_navigation( array(
-					'prev_text' => '<span class="screen-reader-text">' . __( 'Previous Post', 'twentyseventeen' ) . '</span><span aria-hidden="true" class="nav-subtitle">' . __( 'Previous', 'twentyseventeen' ) . '</span> <span class="nav-title"><span class="nav-title-icon-wrapper">' . twentyseventeen_get_svg( array( 'icon' => 'arrow-left' ) ) . '</span>%title</span>',
-					'next_text' => '<span class="screen-reader-text">' . __( 'Next Post', 'twentyseventeen' ) . '</span><span aria-hidden="true" class="nav-subtitle">' . __( 'Next', 'twentyseventeen' ) . '</span> <span class="nav-title">%title<span class="nav-title-icon-wrapper">' . twentyseventeen_get_svg( array( 'icon' => 'arrow-right' ) ) . '</span></span>',
-				) );
-
-			endwhile; // End of the loop.
+			// for no sidebar
+			if ( current_theme_supports( 'theme-layouts' ) && ! is_admin() ) {
+				if ( 'layout-1c' == hybrid_theme_layouts_get_layout() ) {
+					$size = 'large';
+				}
+			}
 			?>
+			<div class="entry-image clearfix" itemprop="image" itemscope itemtype="https://schema.org/ImageObject">
+				<?php the_post_thumbnail( $size, array( 'class' => 'entry-thumbnail', 'alt' => esc_attr( get_the_title() ) ) ); ?>
+				<meta itemprop="url" content="<?php echo esc_url( wp_get_attachment_url( get_post_thumbnail_id( get_the_ID() ) ) ); ?>">
+				<meta itemprop="width" content="800">
+				<meta itemprop="height" content="480">
+			</div>
+		<?php endif; ?>
 
-			<a href="#tlBtn-openModal"><button type="button" class="custom-buttons" data-class="tlbtn_2" data-title="Test" data-request-url="dfasfdsa" data-price="11111">dsfasd</button></a>
+	<?php endif; ?>
 
-			<a href="#tlBtn-openModal"><button type="button" class="custom-buttons" data-class="tlbtn_23" data-title="Tessdfsfsfast" data-request-url="dfasfdsa" data-price="11121212111">dsfasd</button></a>
+	<?php sitebox_entry_publisher(); ?>
+
+	<header class="entry-header wrap">
+
+		<?php the_title( '<h1 ' . hybrid_get_attr( 'entry-title' ) . '>', '</h1>' ); ?>
+
+		<meta itemscope itemprop="mainEntityOfPage" itemType="https://schema.org/WebPage" itemid="<?php the_permalink(); ?>" content="<?php echo esc_attr( get_the_title() ); ?>" />
+
+		<div class="entry-meta clearfix">
+
+			<?php sitebox_posted_on_single(); ?>
+
+			<?php sitebox_entry_author(); ?>
+
+			<?php sitebox_entry_like(); ?>
+
+			<?php sitebox_comment_count_single(); ?>
+
+		</div><!-- .entry-meta -->
+
+	</header>
+
+	<div <?php hybrid_attr( 'entry-content' ); ?>>
+
+		<?php the_content(); ?>
+		<?php
+		wp_link_pages( array(
+			'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'sitebox' ),
+			'after'  => '</div>',
+		) );
+		?>
+
+	</div>
+
+	<a href="#tlBtn-openModal"><button type="button" class="custom-buttons" data-class="tlbtn_2" data-title="Test" data-request-url="dfasfdsa" data-price="11111">dsfasd</button></a>
+
+	<a href="#tlBtn-openModal"><button type="button" class="custom-buttons" data-class="tlbtn_23" data-title="Tessdfsfsfast" data-request-url="dfasfdsa" data-price="11121212111">dsfasd</button></a>
 
 
 
-			<div id="tlBtn-openModal" class="tlBtn-modalDialog">
-				<div>	<a href="#tlBtn-close" title="Close" class="tlBtn-close">X</a>
+	<div id="tlBtn-openModal" class="tlBtn-modalDialog">
+		<div>	<a href="#tlBtn-close" title="Close" class="tlBtn-close">X</a>
 
-					<div id="tlbtn-product-title"><p>Buy <span id="tlbtn-product-name"> </span> | Price <span id="tlbtn-product-price"> </span><p></div>
+			<div id="tlbtn-product-title"><p>View Discounted Offers on <span id="tlbtn-product-name"> </span><p></div>
 
-					<div id="tltbn-main-form">
-						<form action="mailto:asmita.subedi@deerwalk.edu.np" id="test" name="buyform" method="post" enctype="text/plain">
-							<!-- Title: <input type="text" name="btnname"><br>
-                            Price: <input type="text" name="price"><br> -->
-							Name: <input type="text" name="name"><br>
-							Email: <input type="email" name="email"><br>
-							Phone No: <input type="text" name="phno"><br>
+			<div id="tltbn-main-form">
+				<form action="mailto:asmita.subedi@deerwalk.edu.np" id="tlbtnForm" name="buyform" method="post" enctype="text/plain">
+					<!-- Title: <input type="text" name="btnname"><br>
+                    Price: <input type="text" name="price"><br> -->
 
-							<div id="tlbtn-sumit-div">
-								<input id="tlbtn-sumbit" type="submit" value="Submit">
-							</div>
-						</form>
+					<div class="tlbtn-form-field"> <label for="tlbtn-field-name">Name</label> <input id="tlbtn-field-name-input" type="text" required="required" placeholder="Enter your full name" name="name"></div>
+
+					<div class="tlbtn-form-field"> <label for="tlbtn-field-contact">Location</label> <input id="tlbtn-field-name-input" type="text" required="required" placeholder="Enter your address (address and city)" name="location"></div>
+
+
+					<div class="tlbtn-form-field"> <label for="tlbtn-field-contact">Phone</label> <input id="tlbtn-field-name-input" type="text" required="required" placeholder="Enter your hone number (9813XXXXXX or 01-44XXXXX)" name="phone"></div>
+
+
+
+
+					<div id="tlbtn-sumit-div">
+						<div id="tlbtn-sumbit">
+
+							<input  type="button" value="Submit">
+
+						</div>
+
 					</div>
 
+
+
+				</form>
+
+				<div id="tlbtn-text">We value your privacy. Your details are secure with us.</div>
+
+
+			</div>
+
+		</div>
+	</div>
+
+
+
+
+	<div id="tlbtn-openSuccessDialog" class="tlbtn-successDialog">
+		<div id="tlbtn-success-internal">
+			<a href="#tlBtn-close" title="Close" class="tlBtn-close">X</a>
+
+			<div id="tlbtn-success-box">
+				<div id="tlbtn-product-title"><p>Query received successfully!</p></div>
+
+				<div id="tltbn-main-form">
+					<h3>We will call you shortly to provide details on discounted offers.</h3>
+					<h4>Have a good day!</h4>
 				</div>
 			</div>
 
-
-			<script type="text/javascript" src="https://code.jquery.com/jquery-3.1.1.js"></script>
-
-			<script>
-				$(document).ready(function()
-				{
+		</div>
+	</div>
 
 
+	<script type="text/javascript" src="https://code.jquery.com/jquery-3.1.1.js"></script>
+
+	<script>
+		$(document).ready(function()
+		{
+			//global url
+			$url= window.location.href;
+			console.log($url);
+
+			//submit button action
+			$("#tlbtn-sumbit").click(function(){
+
+				FormObject = document.forms['tlbtnForm'];
+
+				var name = FormObject.elements["name"].value;
+				var location = FormObject.elements["location"].value;
+				var phone = FormObject.elements["phone"].value
+
+				//if empty throw error
+				if(name == "" || location == "" || phone ==""){
+					alert("Please, fill up all the fields.");
+				}
+				//on success
+				else {
+
+					//cleaning the $url
+					$url=window.location.origin+window.location.pathname;
+
+					window.location = $url+'#tlbtn-openSuccessDialog';
+
+				}
+				console.log("Name: " + name + " Location: " + location + " Phone: "+ phone);
+			});
+
+			//on close button
+			$(".tlBtn-close").click(function(){
+
+				console.log(window.location.origin+window.location.pathname);
+
+				//cleaning the $url
+				$url=window.location.origin+window.location.pathname;
+			});
 
 
-					//$("#test").hide();
-					$("button").click(function()
-					{
-						var c1 =($(this).data("class"));
-						//alert(c1);
 
-						var c2 = (c1.split("_"));
-						//alert(c2[0]);
+			//$("#test").hide();
+			$("button").click(function()
+			{
+				var c1 =($(this).data("class"));
+				//alert(c1);
 
-						if(c2[0]== "tlbtn"){
-							//alert($(this).data("title"));alert($(this).data("price"));
-							FormObject = document.forms['test'];
-							//	alert(FormObject.elements["btnname"].value = $(this).data("title"));
-							var productName = $(this).data("title");
-							console.log(productName);
-							$('#tlbtn-product-name').html(productName);
-							// FormObject.elements["btnname"].value = $(this).data("title");
+				var c2 = (c1.split("_"));
+				//alert(c2[0]);
 
-							$('#tlbtn-product-price').html = $(this).data("price");
-							//	alert(FormObject.elements["price"].value = $(this).data("price"));
+				if(c2[0]== "tlbtn"){
+					//alert($(this).data("title"));alert($(this).data("price"));
+					FormObject = document.forms['test'];
+					//	alert(FormObject.elements["btnname"].value = $(this).data("title"));
+					var productName = $(this).data("title");
+					console.log(productName);
+					$('#tlbtn-product-name').html(productName);
+					// FormObject.elements["btnname"].value = $(this).data("title");
 
-							//FormObject.elements["price"].value = $(this).data("price");
+					//$('#tlbtn-product-price').html = $(this).data("price");
+					//	alert(FormObject.elements["price"].value = $(this).data("price"));
 
-
-
+					//FormObject.elements["price"].value = $(this).data("price");
 
 
+//							$("#test").show();
+				}
+				else {
+//							$("#test").hide();
+				}
+			});
+		});
+
+	</script>
+
+	<footer class="entry-footer wrap clearfix">
+
+		<?php sitebox_entry_category( true ); ?>
+
+		<?php sitebox_entry_tags(); ?>
+
+		<?php sitebox_entry_share_single(); ?>
+
+	</footer>
 
 
+	<?php if ( function_exists( 'sharing_display' ) ) : ?>
+		<div class="jetpack-share-like">
+			<?php sharing_display( '', true ); ?>
+			<?php if ( class_exists( 'Jetpack_Likes' ) ) { $custom_likes = new Jetpack_Likes; echo $custom_likes->post_likes( '' ); } ?>
+		</div>
+	<?php endif; ?>
 
-
-
-							$("#test").show();
-						}
-						else {
-							$("#test").hide();
-						}
-					});
-				});
-
-			</script>
-		</main><!-- #main -->
-	</div><!-- #primary -->
-	<?php get_sidebar(); ?>
-</div><!-- .wrap -->
-
-<?php get_footer();
+</article><!-- #post-## -->
